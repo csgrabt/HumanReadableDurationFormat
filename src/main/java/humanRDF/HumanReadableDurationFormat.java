c3397b4adc3264ce52656d177b50d9d10ca7340e
Csekő György <gy.cseko@outlook.com>
2021-09-20 13:54:00 +0200

    Initial commit, just started the project and save the web page.

---
+++ src/main/java/humanRDF/HumanReadableDurationFormat.java
@@ -0,0 +1,9 @@
+package humanRDF;
+
+/*
+https://www.codewars.com/kata/human-readable-duration-format?utm_source=newsletter&utm_medium=email&utm_campaign=java_fejleszto_probafeladat&utm_term=2021-09-20
+*/
+
+
+public class HumanReadableDurationFormat {
+}
