620469c46bf9f055eebc0615d60a18fdb774a151
Csekő György <gy.cseko@outlook.com>
2021-09-21 11:20:56 +0200

    same refactor and rename

--- src/main/java/humanRDF/TimeConverter.java
+++ src/main/java/humanRDF/TimeConverter.java
@@ -7,33 +7,32 @@ import java.util.TreeMap;
 
 public class TimeConverter {
 
+    private static List<Units> units = Arrays.stream(Units.values()).toList();
+
     public static Map<Units, Integer> getTheDateInAMap(int numberToBeConverted) {
         Map<Units, Integer> readableDate = new TreeMap<>();
-        List<Units> units = getUnitsAsAList();
-        getTheNumberOfTheYearsAndPutItInTheMap(readableDate, units, numberToBeConverted / units.get(0).getConvertNumberToSecond(), 0);
+        calculateNumberOfYear(readableDate, numberToBeConverted);
         for (int i = 1; i < units.size(); i++) {
-            fillTheMap(numberToBeConverted, readableDate, units, i);
+            calculateTheValueOfTheUnit(numberToBeConverted, readableDate, i);
         }
-
-
         return readableDate;
     }
 
-    private static List<Units> getUnitsAsAList() {
-        return Arrays.stream(Units.values()).toList();
+    private static void calculateNumberOfYear(Map<Units, Integer> readableDate, int numberToBeConverted) {
+        int numberOfYear = numberToBeConverted / units.get(0).getConvertNumberToSecond();
+        putTheValueToTheMap(readableDate, 0, numberOfYear);
+    }
+
+    private static void calculateTheValueOfTheUnit(int numberToBeConverted, Map<Units, Integer> readableDate, int indexOfTheUnit) {
+        int value = (numberToBeConverted % units.get(indexOfTheUnit - 1).getConvertNumberToSecond()) / units.get(indexOfTheUnit).getConvertNumberToSecond();
+        putTheValueToTheMap(readableDate, indexOfTheUnit, value);
 
     }
 
-    private static void getTheNumberOfTheYearsAndPutItInTheMap(Map<Units, Integer> readableDate, List<Units> units, int numberOfYear, int indexOfTheUnit) {
-        if ((numberOfYear) != 0) {
-            readableDate.put(units.get(indexOfTheUnit), numberOfYear);
+    private static void putTheValueToTheMap(Map<Units, Integer> readableDate, int indexOfTheUnit, int value) {
+        if ((value) != 0) {
+            readableDate.put(units.get(indexOfTheUnit), value);
         }
-    }
-
-    private static void fillTheMap(int numberToBeConverted, Map<Units, Integer> date, List<Units> units, int i) {
-        int value = (numberToBeConverted % units.get(i - 1).getConvertNumberToSecond()) / units.get(i).getConvertNumberToSecond();
-        getTheNumberOfTheYearsAndPutItInTheMap(date, units, value, i);
-
     }
 
 }
