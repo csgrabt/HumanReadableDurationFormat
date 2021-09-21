43d047014a90b63058e894bb2f675b2e4cbc78e1
Csekő György <gy.cseko@outlook.com>
2021-09-21 13:41:20 +0200

    improve warnings in the TimeConverter Class

--- src/main/java/humanRDF/TimeConverter.java
+++ src/main/java/humanRDF/TimeConverter.java
@@ -7,31 +7,31 @@ import java.util.TreeMap;
 
 public class TimeConverter {
 
-    private final static List<Units> units = Arrays.stream(Units.values()).toList();
+    private final static List<Units> UNITS = Arrays.stream(Units.values()).toList();
 
     public static Map<Units, Integer> getTheDateInAMap(int numberToBeConverted) {
         Map<Units, Integer> readableDate = new TreeMap<>();
         calculateNumberOfYear(readableDate, numberToBeConverted);
-        for (int i = 1; i < units.size(); i++) {
+        for (int i = 1; i < UNITS.size(); i++) {
             calculateTheValueOfTheUnit(numberToBeConverted, readableDate, i);
         }
         return readableDate;
     }
 
     private static void calculateNumberOfYear(Map<Units, Integer> readableDate, int numberToBeConverted) {
-        int numberOfYear = numberToBeConverted / units.get(0).getConvertNumberToSecond();
+        int numberOfYear = numberToBeConverted / UNITS.get(0).getConvertNumberToSecond();
         putTheValueToTheMap(readableDate, 0, numberOfYear);
     }
 
     private static void calculateTheValueOfTheUnit(int numberToBeConverted, Map<Units, Integer> readableDate, int indexOfTheUnit) {
-        int value = (numberToBeConverted % units.get(indexOfTheUnit - 1).getConvertNumberToSecond()) / units.get(indexOfTheUnit).getConvertNumberToSecond();
+        int value = (numberToBeConverted % UNITS.get(indexOfTheUnit - 1).getConvertNumberToSecond()) / UNITS.get(indexOfTheUnit).getConvertNumberToSecond();
         putTheValueToTheMap(readableDate, indexOfTheUnit, value);
 
     }
 
     private static void putTheValueToTheMap(Map<Units, Integer> readableDate, int indexOfTheUnit, int value) {
         if ((value) != 0) {
-            readableDate.put(units.get(indexOfTheUnit), value);
+            readableDate.put(UNITS.get(indexOfTheUnit), value);
         }
     }
 
