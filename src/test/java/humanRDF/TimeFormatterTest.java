0a8a3540ad3d69cb5b4b5abd3391044f214ed5bb
Csekő György <gy.cseko@outlook.com>
2021-09-21 13:37:35 +0200

    improve warnings in the test class

--- src/test/java/humanRDF/TimeFormatterTest.java
+++ src/test/java/humanRDF/TimeFormatterTest.java
@@ -23,10 +23,11 @@ class TimeFormatterTest {
     @Test
     void timeIsNotValid() {
 
-        Exception ae = assertThrows(IllegalTimeFormatException.class, () ->
-        {
-            TimeFormatter.formatDuration(-1);
-        });
+        IllegalTimeFormatException it = assertThrows(IllegalTimeFormatException.class, () ->
+
+                TimeFormatter.formatDuration(-1)
+        );
+        assertEquals("Time cannot be negative!", it.getMessage());
     }
 
     @Test
@@ -35,7 +36,7 @@ class TimeFormatterTest {
         assertEquals("1 second", TimeFormatter.formatDuration(1));
         assertEquals("1 minute", TimeFormatter.formatDuration(60));
         assertEquals("1 hour", TimeFormatter.formatDuration(3600));
-        assertEquals("1 day", TimeFormatter.formatDuration(3600*24));
+        assertEquals("1 day", TimeFormatter.formatDuration(3600 * 24));
         assertEquals("1 year", TimeFormatter.formatDuration(31_536_000));
 
 
@@ -47,7 +48,7 @@ class TimeFormatterTest {
         assertEquals("2 seconds", TimeFormatter.formatDuration(2));
         assertEquals("2 minutes", TimeFormatter.formatDuration(120));
         assertEquals("2 hours", TimeFormatter.formatDuration(7200));
-        assertEquals("3 days", TimeFormatter.formatDuration(3600*24*3));
+        assertEquals("3 days", TimeFormatter.formatDuration(3600 * 24 * 3));
         assertEquals("2 years", TimeFormatter.formatDuration(63_072_000));
     }
 
@@ -56,7 +57,7 @@ class TimeFormatterTest {
     void differentTimes() {
         assertEquals("1 hour, 1 minute and 2 seconds", TimeFormatter.formatDuration(3662));
         assertEquals("2 years, 10 days, 3 hours, 45 minutes and 25 seconds", TimeFormatter.formatDuration(63_949_525));
-        assertEquals("2 years and 1 second", TimeFormatter.formatDuration(63_072_000+1));
+        assertEquals("2 years and 1 second", TimeFormatter.formatDuration(63_072_000 + 1));
 
     }
 
